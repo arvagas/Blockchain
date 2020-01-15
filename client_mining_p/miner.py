14060301b8227dc14eb300eba6ff7daf1a0db4d8
Arvin Agas <arvagas@gmail.com>
2020-01-15 16:18:54 -0500

    client successfully mines

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,9 +13,9 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    block_string = json.dumps(block)
+    block_string = json.dumps(block, sort_keys=True)
     proof = 0
-    while self.valid_proof(block_string, proof) is False:
+    while valid_proof(block_string, proof) is False:
         proof += 1
 
     # TODO: Return proof
@@ -53,6 +53,9 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
+    # Instantiate coin counter
+    coins_mined = 0
+
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -66,7 +69,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data.get('last_block'))
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -77,4 +80,8 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data.get('message') == 'New Block Forged':
+            coins_mined += 1
+            print(f'Coins mined so far: {coins_mined}')
+        else:
+            print(data.message)
